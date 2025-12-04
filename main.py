26a5339145cdcb33e5b4040c929d65618324e88a
anshumancodes <anshumanprof01@gmail.com>
2025-12-04 09:52:57 +0530

    feat: add tokenization demo with tiktoken

---
+++ main.py
@@ -0,0 +1,5 @@
+import tiktoken;
+encoder=tiktoken.encoding_for_model("gpt-4o")
+text ="Hey there ! how are you doing today"
+tokens=encoder.encode(text)
+print("token generated for text : ",tokens)
