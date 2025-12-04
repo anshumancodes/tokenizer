b4693dbbf7377d04637c36eff5a5ea2d3face342
anshumancodes <anshumanprof01@gmail.com>
2025-12-04 11:00:47 +0530

    refactor: add decoded token output to main script

--- main.py
+++ main.py
@@ -2,4 +2,6 @@ import tiktoken;
 encoder=tiktoken.encoding_for_model("gpt-4o")
 text ="Hey there ! how are you doing today"
 tokens=encoder.encode(text)
-print("token generated for text : ",tokens)
+print("token generated for text : ",tokens);
+decoded=encoder.decode(tokens);
+print("decoded tokens says : ",decoded)
